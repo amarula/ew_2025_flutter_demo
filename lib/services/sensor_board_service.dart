4bf12c414d277d60d00718b398afad1321be456f
Andrea Ricchi <andrea.ricchi@amarulasolutions.com>
2025-01-24 15:35:23 +0100

    Add SensorBoardService class
    Added class to load the dynamic library to communicate via CAN and that
    stores and reloads the data.
    
    Signed-off-by: Andrea Ricchi <andrea.ricchi@amarulasolutions.com>

---
+++ lib/services/sensor_board_service.dart
@@ -0,0 +1,84 @@
+// Dart imports:
+import 'dart:async';
+import 'dart:ffi';
+import 'dart:io' show Directory, File;
+
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:path/path.dart' as path;
+
+typedef BoolFuncCpp = Bool Function();
+typedef BoolFunc = bool Function();
+
+typedef DoubleFuncCpp = Double Function();
+typedef DoubleFunc = double Function();
+
+class SensorBoardService with ChangeNotifier {
+  late Timer _updateTimer;
+
+  double _temperature = 0;
+  double get temperature => _temperature;
+
+  double _humidity = 0;
+  double get humidity => _humidity;
+
+  double _pressure = 0;
+  double get pressure => _pressure;
+
+  bool init() {
+    try {
+      var libraryPath = path.join(
+        Directory.current.path,
+        'ew-2025-flutter-demo-can-lib',
+        'build',
+        'lib',
+        'libfluttercan.so',
+      );
+
+      if (!File(libraryPath).existsSync()) {
+        libraryPath = 'libfluttercan.so';
+      }
+
+      final lib = DynamicLibrary.open(libraryPath);
+
+      final canStartRx = lib
+          .lookup<NativeFunction<BoolFuncCpp>>('can_start_rx')
+          .asFunction<BoolFunc>();
+
+      final getTemperature = lib
+          .lookup<NativeFunction<DoubleFuncCpp>>('get_temperature')
+          .asFunction<DoubleFunc>();
+
+      final getHumidity = lib
+          .lookup<NativeFunction<DoubleFuncCpp>>('get_humidity')
+          .asFunction<DoubleFunc>();
+
+      final getPressure = lib
+          .lookup<NativeFunction<DoubleFuncCpp>>('get_pressure')
+          .asFunction<DoubleFunc>();
+
+      if (!canStartRx()) {
+        return false;
+      }
+
+      _updateTimer = Timer.periodic(const Duration(seconds: 1), (_) {
+        _temperature = getTemperature();
+        _humidity = getHumidity();
+        _pressure = getPressure();
+        notifyListeners();
+      });
+    } catch (e) {
+      // ignore: avoid_print
+      print('Failed to load dynamic library $e');
+      return false;
+    }
+
+    return true;
+  }
+
+  void deinit() {
+    _updateTimer.cancel();
+  }
+}
